c591838cd7bf6e94b73e1ef69b08103499b62337
Matheusrpires <matheusrp97@hotmail.com>
2023-05-21 18:53:55 -0300

    chore: update teste bash

--- teste.sh
+++ teste.sh
@@ -9,7 +9,10 @@ output_file="version_output_$(date +%s%N).txt"
 echo "Running custom logic for $package_new_version"
 # Add your custom logic here for all packages
 
-if [[ $package == *$package_new_version* ]]; then
+echo $package_new_version
+echo $package
+
+if [[ "$package_new_version" == *"$package"* ]]; then
     echo "$package_new_version" >> "$output_file"
     echo "Parameter 1 contains Parameter 2"
 else
