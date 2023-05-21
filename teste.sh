01e1017bceff079595eb2bf1d459c900964daaed
Matheusrpires <matheusrp97@hotmail.com>
2023-05-21 18:36:24 -0300

    chore: adding indepent mode to lerna

---
+++ teste.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Check if parameter 1 contains parameter 2
+
+package_new_version=$1
+package=$2
+output_file="version_output_$(date +%s%N).txt"
+
+echo "Running custom logic for $package_new_version"
+# Add your custom logic here for all packages
+
+if [[ $package == *"$package_new_version"* ]]; then
+    echo "$package_new_version" >> "$output_file"
+    echo "Parameter 1 contains Parameter 2"
+else
+    echo "Parameter 1 does not contain Parameter 2"
+fi
